04f9f5c682cbc60f1e01b50a45cde19fd41f0f29
degel55 <chicubfan@gmail.com>
2016-09-06 19:40:12 -0500

    started REST

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,17 @@
 class ContactsController < ApplicationController
-  def one
-    @contact = Contact.find(2)
-  end  
-
-  def all
+  def index
     @contacts = Contact.all
-  end  
+  end
 
-  def new_contact
+  def new
     
   end
 
-  def create_contact
-    @contact = Contact.create(first_name: params[:first_name], last_name: params[:last_name], phone_number: params[:phone_number], email: params[:email])
+  def create
+    @contact = Contact.create(first_name: params[:first_name],
+                              last_name: params[:last_name],
+                              phone_number: params[:phone_number],
+                              email: params[:email])
+    redirect_to "/contacts/#{@contact.id}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get '/one' => 'contacts#one'
-  get '/all' => 'contacts#all'
+  get '/' => 'contacts#index'
+  get '/contacts' => 'contacts#index'
 
-  get '/new_contact' => 'contacts#new_contact'
-  post '/new_contact' => 'contacts#create_contact'
+  get '/contacts/new' => 'contacts#new'
+  post '/contacts' => 'contacts#create'
+  
 end
