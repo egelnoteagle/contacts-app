ed8a139b9c47bdae7ccc8ca80a4f3d31820e8092
degel55 <chicubfan@gmail.com>
2016-09-07 16:53:24 -0500

    more updates

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -18,4 +18,23 @@ class ContactsController < ApplicationController
   def show
     @contact = Contact.find(params[:id])
   end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    @contact.update(first_name: params[:first_name],
+                    last_name: params[:last_name],
+                    phone_number: params[:phone_number],
+                    email: params[:email])
+    redirect_to "/contacts/#{@contact.id}"
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to "/contacts"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   post '/contacts' => 'contacts#create'
 
   get '/contacts/:id' => 'contacts#show'
-  
+
+  get '/contacts/:id/edit' => 'contacts#edit'
+  patch '/contacts/:id' => 'contacts#update'
+
+  delete '/contacts/:id' => 'contacts#destroy'
 end
